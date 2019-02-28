b0b7105309c62832af9843fa6fd2d7e14e0d76fa
unknown <adautzza@gmail.com>
2019-02-28 21:03:01 +0200

    mesaj

--- JavaHashCode/tests/PictureDistance.java
+++ JavaHashCode/tests/PictureDistance.java
@@ -23,4 +23,60 @@ public class PictureDistance {
 
         assertEquals(a.getDistance(b.getTags()),1);
     }
+    
+    @Test
+    public void name1() {
+        Set<String> a_tag = new HashSet<>();
+        a_tag.add("love");
+        a_tag.add("hate");
+
+        Set<String> b_tag = new HashSet<>();
+        b_tag.add("hate");
+        b_tag.add("garden");
+        b_tag.add("hello");
+        b_tag.add("pizza");
+        Picture a = new Picture(false,a_tag);
+
+        Picture b = new Picture(false, b_tag);
+
+        assertEquals(a.getDistance(b.getTags()),1);
+    }
+    
+    @Test
+    public void name2() {
+        Set<String> a_tag = new HashSet<>();
+        a_tag.add("love");
+        a_tag.add("hate");
+
+        Set<String> b_tag = new HashSet<>();
+        b_tag.add("food");
+        b_tag.add("garden");
+        b_tag.add("hello");
+        b_tag.add("pizza");
+        Picture a = new Picture(false,a_tag);
+
+        Picture b = new Picture(false, b_tag);
+
+        assertEquals(a.getDistance(b.getTags()),0);
+    }
+    
+    @Test
+    public void name3() {
+        Set<String> a_tag = new HashSet<>();
+        a_tag.add("garden");
+        a_tag.add("hate");
+        a_tag.add("hello");
+        a_tag.add("pizza");
+
+        Set<String> b_tag = new HashSet<>();
+        b_tag.add("hate");
+        b_tag.add("garden");
+        b_tag.add("hello");
+        b_tag.add("pizza");
+        Picture a = new Picture(false,a_tag);
+
+        Picture b = new Picture(false, b_tag);
+
+        assertEquals(a.getDistance(b.getTags()),0);
+    }
 }
